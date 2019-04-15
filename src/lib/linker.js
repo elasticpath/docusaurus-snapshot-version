5b720fc7400ba8650dc614d872d52aa7cac2141b
Parth Patel <parth.patel@elasticpath.com>
2019-04-15 11:54:25 -0400

    Implement relative md link replacement

--- src/lib/linker.js
+++ src/lib/linker.js
@@ -4,29 +4,30 @@ const path = require('path');
 const util = require('util');
 const MARKDOWN_PATTERN = path.join("**", "*.md");
 const LINKED_ASSET_REGEX = new RegExp(/(?<=!\[[^\]()'"]*?\]\(assets)\/(?=[\w\s,-]+?\.[A-Za-z]+?\))/, 'g');
+const LINKED_MARKDOWN_REGEX = new RegExp(/(?<=\[[^\]]+?\]\([^)#]+?\.)md(?=#?[^.)]*?\))/, 'g');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 exports.linkAssetsAndMarkdownFiles = (versionedDocsDir, version) => {
-    console.info("Linking versioned asset in versioned markdown files...");
+    console.info("Linking versioned asset and relative links in versioned markdown files...");
     let pathToVDocs = path.join(versionedDocsDir, `version-${version}`, MARKDOWN_PATTERN);
     let files = glob.sync(pathToVDocs);
-    return Promise.all(files.map((file) => linkAssetsInFile(file, version)));
+    return Promise.all(files.map((file) => link(file, version)));
 }
 
-async function linkAssetsInFile(file, version) {
+async function link(file, version) {
     let fileContent = await readFile(file, 'utf8');
-    let linkedFileContent = replaceAssetLinks(fileContent, version);
+    let linkedFileContent = replaceLinks(fileContent, version);
     if (linkedFileContent) {
         await writeFile(file, linkedFileContent, 'utf8');
     }
 }
 
-function replaceAssetLinks(fileContent, version) {
-    if (fileContent && LINKED_ASSET_REGEX.test(fileContent)) {
-        return fileContent.replace(LINKED_ASSET_REGEX, `/version-${version}/`);
-    } else {
+function replaceLinks(fileContent, version) {
+    if (!fileContent) {
         return undefined;
     }
+    let linkedFileContent = fileContent.replace(LINKED_ASSET_REGEX, `/version-${version}/`);
+    return linkedFileContent.replace(LINKED_MARKDOWN_REGEX, "html");
 }
--- test/lib/linkerTest.js
+++ test/lib/linkerTest.js
@@ -10,28 +10,29 @@ describe("Link versioned assets in versioned docs", function() {
         mockfs.restore();
     })
 
-    it("Should replace asset links in versioned docs", async function() {
-        mockUnlinkedAssetMarkdownFiles();
-        await linker.linkAssetsAndMarkdownFiles(path.join(process.cwd(), "versioned_docs"), "1.4.5");
-        let expectation = createLinkedMarkdownContent();
-        let actual = readLinkedMarkdownFiles();
-        assert.strictEqual(actual.docsOverview, expectation.docsOverview);
-        assert.strictEqual(actual.apiOverview, expectation.apiOverview);
-        assert.strictEqual(actual.apiPostCart, expectation.apiPostCart);
+    it("Should replace asset and markdown links in versioned docs", 
+        async function() {
+            mockUnlinkedMarkdownFiles();
+            await linker.linkAssetsAndMarkdownFiles(path.join(process.cwd(), "versioned_docs"), "1.4.5");
+            let expectation = createLinkedMarkdownContent();
+            let actual = readLinkedMarkdownFiles();
+            assert.strictEqual(actual.docsOverview, expectation.docsOverview);
+            assert.strictEqual(actual.apiOverview, expectation.apiOverview);
+            assert.strictEqual(actual.apiPostCart, expectation.apiPostCart);
     });
 
-    it("Should not write to a file when it does not have asset links", 
+    it("Should not write to a file when it does not have links", 
         async function() {
         mockfs({
             "versioned_docs": {
                 "version-1.4.5": {
-                    "Overview.md": "This file do not have assets"
+                    "Overview.md": "This file do not have assets or links to other markdown files"
                 }
             }
         })
         let versionedDocsDir = path.join(process.cwd(), "versioned_docs");
         await linker.linkAssetsAndMarkdownFiles(versionedDocsDir, "1.4.5");
-        let expectation = "This file do not have assets";
+        let expectation = "This file do not have assets or links to other markdown files";
         let actual = fs.readFileSync(path.join(versionedDocsDir, "version-1.4.5", "Overview.md"), 'utf8');
         assert.strictEqual(actual, expectation);
     });
@@ -40,30 +41,30 @@ describe("Link versioned assets in versioned docs", function() {
         mockfs({
             "versioned_docs": {
                 "version-1.4.5": {
-                    "Overview.txt": "This file is not markdown ![Do not replace](doNotReplace.jpeg)"
+                    "Overview.txt": "This file is not markdown ![Do not replace](doNotReplace.jpeg) [anotherMDLink](another.md)"
                 }
             }
         })
         let versionedDocsDir = path.join(process.cwd(), "versioned_docs");
         await linker.linkAssetsAndMarkdownFiles(versionedDocsDir, "1.4.5");
-        let expectation = "This file is not markdown ![Do not replace](doNotReplace.jpeg)";
+        let expectation = "This file is not markdown ![Do not replace](doNotReplace.jpeg) [anotherMDLink](another.md)";
         let actual = fs.readFileSync(path.join(versionedDocsDir, "version-1.4.5", "Overview.txt"), 'utf8');
         assert.strictEqual(actual, expectation);
     });
 
     function createUnlinkedMarkdownContent() {
         return {
-            "apiOverview": "This is an overview for page ![Overview of API](assets/overview.png)",
-            "apiPostCart": "Cart can either be posted as ![post one cart](assets/postone.jpeg) or as ![post multiple carts](assets/postMultiple.gif)",
-            "docsOverview": "The following text is garbage just to test regex: abcd!![def](/feg.png) !(no a link) !![replace1](assets/replaceThis.BC) ![not replace][do not replace](assets/doNotReplace.jpeg) ![[MyImage]](assets/donotReplace.jpeg)"
+            "apiOverview": "This is an overview for page ![Overview of API](assets/overview.png). This is a link to another doc [another](abc.md)",
+            "apiPostCart": "Cart can either be posted as ![post one cart](assets/postone.jpeg) or as ![post multiple carts](assets/postMultiple.gif). For details please refer to [postDetails](../postDetails.md).",
+            "docsOverview": "The following text is garbage just to test regex: abcd!![def](/feg.png) !(no a link) !![replace1](assets/replaceThis.BC) ![not replace][do not replace](assets/doNotReplace.jpeg) ![[MyImage]](assets/donotReplace.jpeg). [[Do not replace]](doNotReplace.md) [Replace content section](abc.md#section2) [Replace content](../abcd.md#def) [ReplaceContent](./abc.md)"
         }
     }
 
     function createLinkedMarkdownContent() {
         return {
-            "apiOverview": "This is an overview for page ![Overview of API](assets/version-1.4.5/overview.png)",
-            "apiPostCart": "Cart can either be posted as ![post one cart](assets/version-1.4.5/postone.jpeg) or as ![post multiple carts](assets/version-1.4.5/postMultiple.gif)",
-            "docsOverview": "The following text is garbage just to test regex: abcd!![def](/feg.png) !(no a link) !![replace1](assets/version-1.4.5/replaceThis.BC) ![not replace][do not replace](assets/doNotReplace.jpeg) ![[MyImage]](assets/donotReplace.jpeg)"
+            "apiOverview": "This is an overview for page ![Overview of API](assets/version-1.4.5/overview.png). This is a link to another doc [another](abc.html)",
+            "apiPostCart": "Cart can either be posted as ![post one cart](assets/version-1.4.5/postone.jpeg) or as ![post multiple carts](assets/version-1.4.5/postMultiple.gif). For details please refer to [postDetails](../postDetails.html).",
+            "docsOverview": "The following text is garbage just to test regex: abcd!![def](/feg.png) !(no a link) !![replace1](assets/version-1.4.5/replaceThis.BC) ![not replace][do not replace](assets/doNotReplace.jpeg) ![[MyImage]](assets/donotReplace.jpeg). [[Do not replace]](doNotReplace.md) [Replace content section](abc.html#section2) [Replace content](../abcd.html#def) [ReplaceContent](./abc.html)"
         } 
     }
 
@@ -80,7 +81,7 @@ describe("Link versioned assets in versioned docs", function() {
         };
     }
 
-    function mockUnlinkedAssetMarkdownFiles() {
+    function mockUnlinkedMarkdownFiles() {
         let files = createUnlinkedMarkdownContent();
         mockfs({
             "versioned_docs": {
