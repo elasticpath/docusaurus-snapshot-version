6185a62d18e51df808e53d237bb94b35e43a1457
Linden Kyaw <linden.kyaw@elasticpath.com>
2021-11-17 01:15:20 -0800

    Refactor commander to specify paramters in CLI when assigning values to parameters

--- src/snapshotVersion.js
+++ src/snapshotVersion.js
@@ -3,9 +3,19 @@ const createVersion = require('./lib/createVersion.js');
 
 module.exports = (argsParser = process.argv) => {
     program
-        .command('create <version> [siteDir]')
-        .action((version, siteDir = ".") => {
-            createVersion.create(version, siteDir);
+        .command('create')
+        .storeOptionsAsProperties()
+        .requiredOption('--version <type>', 'The value of the new version to create.')
+        .option('--siteDir <type>', 'The absolute or relative path to the `website` directory of the Docusaurus V1 project.', '.')
+        .option('--assetType <type...>', 'The file type of the static file type to be versioned.')
+        .action((options) => {
+            let version = options.version;
+            let siteDir = options.siteDir;
+            let assetType = options.assetType;
+            if (!assetType) {
+                assetType = [];
+            }
+            createVersion(version, siteDir, assetType);
         });
     program.parse(argsParser);
 }
