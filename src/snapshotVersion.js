ebc7d27564f56791f38690d2cf40e78c0238c3cf
Linden Kyaw <linden.kyaw@elasticpath.com>
2021-11-17 09:32:00 -0800

    Set store options as properties to store it as the commander object's property.

--- src/snapshotVersion.js
+++ src/snapshotVersion.js
@@ -4,7 +4,7 @@ const createVersion = require('./lib/createVersion.js');
 module.exports = (argsParser = process.argv) => {
     program
         .command('create')
-        .storeOptionsAsProperties()
+        .storeOptionsAsProperties(true)
         .requiredOption('--version <type>', 'The value of the new version to create.')
         .option('--siteDir <type>', 'The absolute or relative path to the `website` directory of the Docusaurus V1 project.', '.')
         .option('--assetType <type...>', 'The file type of the static file type to be versioned.')
