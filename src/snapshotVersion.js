c5e9a6a6e3e22cc60b1aeb9e7ad13693e86307cb
Linden Kyaw <linden.kyaw@elasticpath.com>
2021-12-02 09:22:08 -0800

    Move options variables inside the create function since the standalone variables are not being used again. Change staticDir description.

--- src/snapshotVersion.js
+++ src/snapshotVersion.js
@@ -6,12 +6,9 @@ module.exports = (argsParser = process.argv) => {
         .command('create')
         .requiredOption('--version <type>', '[required] The value of the new version to create.')
         .option('--siteDir <type>', '[optional] The absolute or relative path to the `website` directory of the Docusaurus V1 project.', '.')
-        .option('--staticDir <type...>', '[optional] The name of the static asset directory to be versioned from the static directory in the docs repository', [])
+        .option('--staticDir <type...>', '[optional] The name of the static asset directory to be versioned under the website/static directory', [])
         .action((options) => {
-            let version = options.version;
-            let siteDir = options.siteDir;
-            let staticDir = options.staticDir;
-            createVersion.create(version, siteDir, staticDir);
+            createVersion.create(options.version, options.siteDir, options.staticDir);
         });
     program.parse(argsParser);
 }
